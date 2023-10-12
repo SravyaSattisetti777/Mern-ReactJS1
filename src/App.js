e7f46c5253a01f9a233e7d39e7c635ef27d411e3
K Ganga Vara Prasad <sravyanaidu887@gmail.com>
2023-10-12 15:59:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//------------------------------------------------------------------------------------------------------
+
+import React, { useState } from 'react';
+import './App.css'; // Make sure to import your CSS file
 
 function App() {
+  const [paragraph, setParagraph] = useState('');
+
+  const wordCount = paragraph.split(/\s+/).filter(word => word !== '').length;
+
+  const handleParagraphChange = (e) => {
+    setParagraph(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <div className="white-box">
+          <h1>Responsive Paragraph Word <br /> Counter</h1>
+            <textarea
+              placeholder="Type your paragraph here..."
+              value={paragraph}
+              onChange={handleParagraphChange}
+              style={{ width: '100%', minHeight: '120px', border: '2px solid black', borderRadius: '4px',}}
+            />
+          <p>Word Count: {wordCount}</p>
+        </div>
+      </div>
     </div>
   );
 }
